c64941c64c3a67be8d2bdcc8d529d6b22a82b4e8
w361214206 <58541532+w361214206@users.noreply.github.com>
2024-12-23 12:44:48 +0800

    Add files via upload

---
+++ flaskServer.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from router import f_router
+from base import ObjSql;
+from sqlalchemy import Column,String,Integer
+class Server():
+   def __init__(self):
+      self.o_app =  Flask(__name__);
+   def f_router(self):
+      f_router(self);
+   def f_ObjSql(self):
+      self.o_objSql = ObjSql();
+      def f_table(Base):
+         class User(Base):
+            __tablename__ = "user";
+            uid = Column(Integer, primary_key=True, autoincrement=True) # primary_key表示主键 autoincrement表示自增长
+            name = Column(String(32));#, unique=True
+            gender = Column(String(32));
+         return User;	
+      self.o_objSql.f_creTable(f_table);
+   def f_main(self):
+      self.f_router();
+      self.f_ObjSql();
+      self.o_app.run(debug=True);
+      
+o_server = Server();
+o_server.f_main();
